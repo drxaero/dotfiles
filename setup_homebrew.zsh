e5079d2f61d6677a8d0db38e5b78dfb6471b7ce5
Janus Cheng <drxaero@gmail.com>
2025-03-27 10:17:54 +0800

    [PROD][BEHAVIOR_CHANGED] When `brew` is already installed, update the `brew` rather than skip it.

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -3,13 +3,15 @@
 echo "\n=== Starting Homebrew Setup ===\n"
 
 if cmd_exists brew; then
-  echo "brew is already installed"
+  echo "updating the installed brew..."
+  brew update
 else
   echo "brew is not found, installing brew..."
 
   # copied form https://brew.sh
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
 
 # 安裝 Brewfile 中的軟體, 用 `$ brew bundle dump --describe --force` 來產生 Brewfile
 brew bundle --verbose
